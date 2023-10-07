9b4ab0c2630168fe0d52f7232737c085967ee79a
Gustavo Henrique <gustavo@n2solucoes.com.br>
2023-10-07 14:14:37 -0300

    🚧 Ajustes nas rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'signup']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum', 'auth'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::middleware(['teams_permission'])->group(function () {
+//        Route::get('users', [UserController::class, 'index']);
+        Route::apiResource('users', UserController::class);
+    });
+
+});
+
+//Route::resource('/users', 'App\Http\Controllers\UserController');
