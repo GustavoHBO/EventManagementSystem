c9ef7e5907b755ed8f3603834019074762460f11
Gustavo Henrique <gustavo@n2solucoes.com.br>
2023-10-07 14:10:04 -0300

    🚸 Middleware para os times e para a autenticação.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    /**
+     * Handle an incoming request.
+     * @throws AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+//        session(['team_id' => 0]); // Modify this to get the team_id from the request.
+        return parent::handle($request, $next, ...$guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
