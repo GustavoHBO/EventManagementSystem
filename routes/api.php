ca8d926bbffe1a210bad2b805a8f3df144d576e7
Gustavo Henrique <gustavo@n2solucoes.com.br>
2023-10-16 23:59:59 -0300

    ✨ Adicionando novas rotas.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\LotController;
+use App\Http\Controllers\API\OrderController;
+use App\Http\Controllers\API\SectorController;
+use App\Http\Controllers\API\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -23,14 +28,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum', 'auth'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
     Route::middleware(['teams_permission'])->group(function () {
-//        Route::get('users', [UserController::class, 'index']);
         Route::apiResource('users', UserController::class);
+        Route::apiResource('events', EventController::class);
+        Route::apiResource('sectors', SectorController::class);
+        Route::apiResource('lots', LotController::class);
+        Route::apiResource('tickets', TicketController::class);
+        Route::apiResource('orders', OrderController::class);
     });
 
+    // Roles
+    Route::get('/roles', 'API\RolePermissionController@getRoles');
+    Route::post('/roles', 'API\RolePermissionController@createRole');
+    Route::delete('/roles/{role}', 'API\RolePermissionController@deleteRole');
+
+    // Permissions
+    Route::get('/permissions', 'API\RolePermissionController@getPermissions');
+    Route::post('/permissions', 'API\RolePermissionController@createPermission');
+    Route::delete('/permissions/{permission}', 'API\RolePermissionController@deletePermission');
+
+    // Attach/Detach Permissions to/from Roles
+    Route::put('/roles/{role}/permissions', 'API\RolePermissionController@attachPermissions');
+    Route::delete('/roles/{role}/permissions', 'API\RolePermissionController@detachPermissions');
 });
 
 //Route::resource('/users', 'App\Http\Controllers\UserController');
